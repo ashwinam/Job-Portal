f8c34adc7715e393e541f29ff3a770b211c0ec05
ashwin <ashwinam31@gmail.com>
2022-04-23 19:57:08 +0530

    Implementing Landing Page

--- Job_Portal/urls.py
+++ Job_Portal/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
     # path('posts/', include('posts.urls')),
 ]
